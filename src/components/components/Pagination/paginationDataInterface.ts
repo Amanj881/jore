83a177a159c932a3c58918053db8561638eefc97
Amanj881 <jainaman091996@gmail.com>
2020-10-11 10:58:31 +0530

    fresh

--- src/components/components/Pagination/paginationDataInterface.ts
+++
@@ -1,4 +0,0 @@
-export interface PaginationData {
-  currentPage: number;
-  totalPages: number;
-}
--- src/components/components/Pagination/paginationPropsInterface.ts
+++
@@ -1,6 +0,0 @@
-import { PaginationData } from "./paginationDataInterface";
-
-export interface PaginationProps {
-  paginationData: PaginationData;
-  handlePageChangeCallback: (pageNo: number) => void;
-}
