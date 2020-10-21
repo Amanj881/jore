d4416a5c4c94d5291e95001209c75897917521db
Amanj881 <jainaman091996@gmail.com>
2020-10-21 15:46:17 +0530

    Updates

---
+++ precache-manifest.5e09c9849b18a2d015021641dcc3d6df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87e85313d6cc805f93fc96ad8d42143f",
+    "url": "/journal/index.html"
+  },
+  {
+    "revision": "2fd4e90f87292bf0a18b",
+    "url": "/journal/static/css/main.68363bc6.chunk.css"
+  },
+  {
+    "revision": "71d500227588ec23d666",
+    "url": "/journal/static/js/2.23735bd7.chunk.js"
+  },
+  {
+    "revision": "fa24d2988fd9ee29f9cba16979460922",
+    "url": "/journal/static/js/2.23735bd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fd4e90f87292bf0a18b",
+    "url": "/journal/static/js/main.3a00ce78.chunk.js"
+  },
+  {
+    "revision": "11e6685158c883a55e6e",
+    "url": "/journal/static/js/runtime-main.6b7747ba.js"
+  },
+  {
+    "revision": "5dc7687fd072a64f9c8cc5aca8d48b93",
+    "url": "/journal/static/media/pub.5dc7687f.png"
+  }
+]);
